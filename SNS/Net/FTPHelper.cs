4f7e2d7aeb1ab80d68ad7c7f4677b291986b6f4d
KJongHyun <whdgus8219@naver.com>
2018-08-03 15:34:38 +0900

    20180803 - Framework(FTPHelper 작업2)

--- SNS/Net/FTPHelper.cs
+++ SNS/Net/FTPHelper.cs
@@ -1,39 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Configuration;
 using System.IO;
 
 
 namespace SNS.Net
 {
-    public string FileName { get; set; }
-    private FtpWebRequest request;
-    public FTPHelper()
+    public class FTPHelper
     {
-    }
-
-    public List<string> DownLoadFile(string FileName)
-    {
-        this.FileName = FileName;
-        request = (FtpWebRequest)WebRequest.Create(this.FileName);
-        request.Method = WebRequestMethods.Ftp.DownloadFile;
-        request.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["FTP.User"], ConfigurationManager.AppSettings["FTP.Password"]);
-
-        using (FtpWebResponse resp = (FtpWebResponse)request.GetResponse())
+        public string FileName { get; set; }
+        private FtpWebRequest request;
+        public FTPHelper()
         {
-            Stream stream = resp.GetResponseStream();
-            FileStream writeStream = new FileStream(ConfigurationManager.AppSettings["FTP.DownLoadPath"] + FileName, FileMode.Create);
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                data.Add(reader.ReadToEnd());
-            }
         }
 
-    }
+        public List<string> DownLoadFile(string FileName)
+        {
+            this.FileName = FileName;
+            request = (FtpWebRequest)WebRequest.Create(this.FileName);
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            request.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["FTP.User"], ConfigurationManager.AppSettings["FTP.Password"]);
 
+            using (FtpWebResponse resp = (FtpWebResponse)request.GetResponse())
+            {
+                Stream stream = resp.GetResponseStream();
+                FileStream writeStream = new FileStream(ConfigurationManager.AppSettings["FTP.DownLoadPath"] + FileName, FileMode.Create);
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    data.Add(reader.ReadToEnd());
+                }
+            }
+
+        }
+    }
 
 }
