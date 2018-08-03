a451990dede33afe070521e1643664f1d68aa974
KJongHyun <whdgus8219@naver.com>
2018-08-03 15:31:18 +0900

    20180803 - Framework(FTPHelper 작업)

---
+++ SNS/Net/FTPHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+
+
+namespace SNS.Net
+{
+    public string FileName { get; set; }
+    private FtpWebRequest request;
+    public FTPHelper()
+    {
+    }
+
+    public List<string> DownLoadFile(string FileName)
+    {
+        this.FileName = FileName;
+        request = (FtpWebRequest)WebRequest.Create(this.FileName);
+        request.Method = WebRequestMethods.Ftp.DownloadFile;
+        request.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["FTP.User"], ConfigurationManager.AppSettings["FTP.Password"]);
+
+        using (FtpWebResponse resp = (FtpWebResponse)request.GetResponse())
+        {
+            Stream stream = resp.GetResponseStream();
+            FileStream writeStream = new FileStream(ConfigurationManager.AppSettings["FTP.DownLoadPath"] + FileName, FileMode.Create);
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                data.Add(reader.ReadToEnd());
+            }
+        }
+
+    }
+
+
+}
