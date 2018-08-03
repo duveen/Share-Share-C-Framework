6846dbf79c21577caf670b121cc7a220d29d171b
KJongHyun <whdgus8219@naver.com>
2018-08-03 15:50:21 +0900

    20180803 - Framework(FTPHelper 작업3)

--- SNS/Net/FTPHelper.cs
+++ SNS/Net/FTPHelper.cs
@@ -2,33 +2,51 @@
 using System.Net;
 using System.Configuration;
 using System.IO;
-
+using System;
 
 namespace SNS.Net
 {
     public class FTPHelper
     {
+        private readonly int Length = 2048;
+
         public string FileName { get; set; }
         private FtpWebRequest request;
         public FTPHelper()
         {
         }
 
-        public List<string> DownLoadFile(string FileName)
+        public bool DownLoadFile(string FileName)
         {
             this.FileName = FileName;
-            request = (FtpWebRequest)WebRequest.Create(this.FileName);
-            request.Method = WebRequestMethods.Ftp.DownloadFile;
-            request.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["FTP.User"], ConfigurationManager.AppSettings["FTP.Password"]);
-
-            using (FtpWebResponse resp = (FtpWebResponse)request.GetResponse())
+            try
             {
-                Stream stream = resp.GetResponseStream();
-                FileStream writeStream = new FileStream(ConfigurationManager.AppSettings["FTP.DownLoadPath"] + FileName, FileMode.Create);
-                using (StreamReader reader = new StreamReader(stream))
+                request = (FtpWebRequest)WebRequest.Create(this.FileName);
+                request.Method = WebRequestMethods.Ftp.DownloadFile;
+                request.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["FTP.User"], ConfigurationManager.AppSettings["FTP.Password"]);
+
+                using (FtpWebResponse resp = (FtpWebResponse)request.GetResponse())
                 {
-                    data.Add(reader.ReadToEnd());
+                    Stream responseStream = resp.GetResponseStream();
+                    FileStream writeStream = new FileStream(ConfigurationManager.AppSettings["FTP.DownLoadPath"] + FileName, FileMode.Create);
+                    byte[] buffer = new byte[Length];
+                    int bytesRead = 0;
+
+                    while (responseStream.Position < responseStream.Length)
+                    {
+                        bytesRead = responseStream.Read(buffer, 0, Length);
+                        writeStream.Write(buffer, 0, bytesRead);
+                    }
+
+                    responseStream.Close();
+                    writeStream.Close();
                 }
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
             }
 
         }
