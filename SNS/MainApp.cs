e83111a3ef54792b01812bab39dd2470efae5516
KJongHyun <whdgus8219@naver.com>
2018-08-03 16:55:11 +0900

    20180803 - Framework(FTPHelper 3차)

---
+++ SNS/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using SNS.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNS
+{
+    class MainApp
+    {
+        public static void Main(String[] args)
+        {
+            FTPHelper fh = new FTPHelper();
+            fh.DownLoadFile("2018-06-05.log");
+        }
+    }
+}
--- SNS/Net/FTPHelper.cs
+++ SNS/Net/FTPHelper.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Configuration;
 using System.IO;
 using System;
+using WinSCP;
 
 namespace SNS.Net
 {
@@ -12,42 +13,96 @@ namespace SNS.Net
 
         public string FileName { get; set; }
         private FtpWebRequest request;
+
+        private Session SESSION;
+
+
         public FTPHelper()
         {
-        }
+            SessionOptions sessionOptions = new SessionOptions
+            {
+                Protocol = Protocol.Ftp,
+                HostName = ConfigurationManager.AppSettings["FTP.HostName"],
+                PortNumber = Convert.ToInt32(ConfigurationManager.AppSettings["FTP.PortNumber"]),
+                UserName = ConfigurationManager.AppSettings["FTP.UserName"],
+                Password = ConfigurationManager.AppSettings["FTP.Password"]
+            };
 
-        public bool DownLoadFile(string FileName)
-        {
-            this.FileName = FileName;
+            SESSION = new Session();
             try
             {
-                request = (FtpWebRequest)WebRequest.Create(ConfigurationManager.AppSettings["FTP_URL"] + this.FileName);
-                request.Method = WebRequestMethods.Ftp.DownloadFile;
-                request.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["FTP.User"], ConfigurationManager.AppSettings["FTP.Password"]);
-
-                using (FtpWebResponse resp = (FtpWebResponse)request.GetResponse())
-                {
-                    Stream responseStream = resp.GetResponseStream();
-                    FileStream writeStream = new FileStream(ConfigurationManager.AppSettings["FTP.DownLoadPath"] + FileName, FileMode.Create);
-                    byte[] buffer = new byte[Length];
-                    int bytesRead = 0;
-
-                    while (responseStream.Position < responseStream.Length)
-                    {
-                        bytesRead = responseStream.Read(buffer, 0, Length);
-                        writeStream.Write(buffer, 0, bytesRead);
-                    }
-
-                    responseStream.Close();
-                    writeStream.Close();
-                }
-                return true;
+                SESSION.Open(sessionOptions);
             }
-            catch(Exception e)
+            catch { throw; }
+        }
+
+        public FTPHelper(string host, int port, string user, string password)
+        {
+            SessionOptions sessionOptions = new SessionOptions
             {
-                Console.WriteLine(e);
-                return false;
+                Protocol = Protocol.Ftp,
+                HostName = host,
+                PortNumber = port,
+                UserName = user,
+                Password = password
+            };
+
+            SESSION = new Session();
+            try
+            {
+                SESSION.Open(sessionOptions);
             }
+            catch { throw; }
+        }
+
+        public List <string> DownLoadFile(string FileName, bool remove = false)
+        {
+            List<string> rstList = new List<string>();
+            TransferOptions transferOptions = new TransferOptions();
+            transferOptions.TransferMode = TransferMode.Binary;
+
+            TransferOperationResult transferResult;
+            transferResult = SESSION.GetFiles((ConfigurationManager.AppSettings["FTP_URL"] + FileName), (ConfigurationManager.AppSettings["FTP.DownLoadPath"] + FileName), remove, transferOptions);
+
+            transferResult.Check();
+
+            foreach (TransferEventArgs transfer in transferResult.Transfers)
+            {
+                rstList.Add(transfer.FileName);
+            }
+
+            return rstList;
+
+            //    this.FileName = FileName;
+            //    try
+            //    {
+            //        request = (FtpWebRequest)WebRequest.Create(ConfigurationManager.AppSettings["FTP_URL"] + this.FileName);
+            //        request.Method = WebRequestMethods.Ftp.DownloadFile;
+            //        request.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["FTP.User"], ConfigurationManager.AppSettings["FTP.Password"]);
+
+            //        using (FtpWebResponse resp = (FtpWebResponse)request.GetResponse())
+            //        {
+            //            Stream responseStream = resp.GetResponseStream();
+            //            FileStream writeStream = new FileStream(ConfigurationManager.AppSettings["FTP.DownLoadPath"] + FileName, FileMode.Create);
+            //            byte[] buffer = new byte[Length];
+            //            int bytesRead = 0;
+
+            //            while (responseStream.Position < responseStream.Length)
+            //            {
+            //                bytesRead = responseStream.Read(buffer, 0, Length);
+            //                writeStream.Write(buffer, 0, bytesRead);
+            //            }
+
+            //            responseStream.Close();
+            //            writeStream.Close();
+            //        }
+            //        return true;
+            //    }
+            //    catch(Exception e)
+            //    {
+            //        Console.WriteLine(e);                
+            //        return false;
+            //    }
 
         }
     }
