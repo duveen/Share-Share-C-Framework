7f7f674ef6a7edd605e38f61cb0ad890b1c5d3e6
duveen <duveen@duveen.me>
2018-08-17 10:10:33 +0900

    20180817 - SF Description add

--- SNS/DataObject/SF/S1F3.cs
+++ SNS/DataObject/SF/S1F3.cs
@@ -12,7 +12,8 @@ namespace SNS.DataObject.SF
     {
         public S1F3()
         {
-            this.CODE_NAME = "S1F3";
+            this.CODE_NAME = "S1, F3 Selected Equipment Status Request";
+            this.DESCRIPTION = "Used to request the values of selected status variables.";
         }
     }
 }
--- SNS/DataObject/SF/StreamFuction.cs
+++ SNS/DataObject/SF/StreamFuction.cs
@@ -10,14 +10,13 @@ namespace SNS.DataObject.SF
 
     [DataContract]
     #region [ KnoewType ]    
-    [KnownType(typeof(S10F3))]
-    [KnownType(typeof(S10F4))]
+    [KnownType(typeof(S1F3))]
+    [KnownType(typeof(S1F4))]
     [KnownType(typeof(S1F13))]
     [KnownType(typeof(S1F14))]
     [KnownType(typeof(S1F17))]
     [KnownType(typeof(S1F18))]
-    [KnownType(typeof(S1F3))]
-    [KnownType(typeof(S1F4))]
+
     [KnownType(typeof(S2F33))]
     [KnownType(typeof(S2F34))]
     [KnownType(typeof(S2F35))]
@@ -26,22 +25,25 @@ namespace SNS.DataObject.SF
     [KnownType(typeof(S2F38))]
     [KnownType(typeof(S2F41))]
     [KnownType(typeof(S2F42))]
+
     [KnownType(typeof(S5F1))]
+
     [KnownType(typeof(S6F11))]
+
     [KnownType(typeof(S7F1))]
     [KnownType(typeof(S7F2))]
     [KnownType(typeof(S7F3))]
     [KnownType(typeof(S7F4))]
     [KnownType(typeof(S7F5))]
     [KnownType(typeof(S7F6))]
+
+    [KnownType(typeof(S10F3))]
+    [KnownType(typeof(S10F4))]
     #endregion
     public class StreamFuction
     {
         [DataMember]
-        public string CODE_NAME { get; set; }
-        public override string ToString()
-        {
-            return CODE_NAME;
-        }
+        public string CODE_NAME { get; set; } 
+        public string DESCRIPTION { get; set; }
     }
 }
